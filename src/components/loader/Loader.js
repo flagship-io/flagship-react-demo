5ea7132d1fca84398657ff0b9c75c66a4133a82a
Heihokon <neoshark22@gmail.com>
2022-08-30 17:07:02 +0100

    add loader component

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,11 @@
+import './Loader.scss'
+
+function Loader() {
+    return (
+        <div className='loader'>
+            <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+        </div >
+    )
+}
+
+export default Loader
