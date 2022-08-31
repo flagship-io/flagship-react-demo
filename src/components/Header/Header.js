8fa38e53521be96733892692be1dc22df9b6937a
Heihokon <neoshark22@gmail.com>
2022-08-31 16:31:33 +0100

    refactor header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,14 @@
 import { useFsFlag } from '@flagship.io/react-sdk'
-import './Header.scss'
+import styles from './Header.module.scss'
 
 function Header() {
     const paymentHeaderTitle = useFsFlag("payment_header_title_text", "Demo app")
     const paymentHeaderColor = useFsFlag("payment_header_color", "red")
     return (
-        <div className="header" style={{ background: paymentHeaderColor.getValue() }}>
-            <div className='title'>{paymentHeaderTitle.getValue()}</div>
-            <a href='/'>
-                <img src='social_github_icon.svg' alt='github' />
+        <div className={styles.header} style={{ background: paymentHeaderColor.getValue() }}>
+            <div className={styles.title}>{paymentHeaderTitle.getValue()}</div>
+            <a target={'_blank'} href='https://github.com/flagship-io/flagship-react-demo' rel="noreferrer">
+                <img className={styles["icon-github"]} src='social_github_icon.svg' alt='github' />
             </a>
         </div>)
 }
