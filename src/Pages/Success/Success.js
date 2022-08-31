be879496faa98cffb7e97b1f2e523efb9aa35f32
Heihokon <neoshark22@gmail.com>
2022-08-31 16:31:13 +0100

    add success page

---
+++ src/Pages/Success/Success.js
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+import styles from './Success.module.scss'
+
+function Success() {
+    return (
+        <div className={styles.success}>
+            <div className={styles.circle}>
+                <img src='/credit-card-check.svg' alt='credit card' />
+            </div>
+            <div className={styles.message}>Payment successfully completed</div>
+            <Link to="/"><i className='fa-regular fa-arrow-left-long' /> Go back to the store</Link>
+        </div>
+    )
+}
+
+export default Success
