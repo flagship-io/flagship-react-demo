ee7853175acb56f7972bcbce5e36ae59f291d131
Heihokon <neoshark22@gmail.com>
2022-09-21 15:48:19 +0100

    add credential component

--- src/Pages/Credential/Credential.js
+++ src/Pages/Credential/Credential.js
@@ -1,20 +1,51 @@
+import { useContext, useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { appContext } from '../../App'
 import './Credential.scss'
 
+const FormInput = ({ name, label, value, onChange }) => {
+    return (
+        <div>
+            <label htmlFor={name} >{label}</label>
+            <input required name={name} value={value} onChange={(e) => {
+                onChange(e.target.value)
+            }} />
+        </div>
+    )
+}
+
 export default function Credential() {
+    const [data, setData] = useState({
+        apiKey: "",
+        envId: ""
+    })
+
+
+    const navigate = useNavigate();
+    const context = useContext(appContext)
+
+    useEffect(() => {
+        setData(context.credential)
+    }, [context])
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        context.setCredential(data)
+        navigate('payment')
+    }
     return (
         <div className="main credential">
             <div className='title'>
                 Flagship Credentials
             </div>
-            <form>
-                <div>
-                    <label htmlFor="apiKey" >Api Key</label>
-                    <input name="apiKey" />
-                </div>
-                <div>
-                    <label htmlFor="envId" >Env ID</label>
-                    <input name="envId" />
-                </div>
+            <form method="post" onSubmit={onSubmit}>
+                <FormInput name="apiKey" label="Api Key" value={data.apiKey} onChange={(value) => {
+                    setData(item => ({ ...item, apiKey: value }))
+                }} />
+                <FormInput name="envId" label="Env ID" value={data.envId} onChange={(value) => {
+                    setData(item => ({ ...item, envId: value }))
+                }} />
+                <button>Submit</button>
             </form>
         </div>)
 }
